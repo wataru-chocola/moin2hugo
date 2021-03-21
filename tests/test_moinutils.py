5782bc21b12095e61b2cd38417e78e21252daf42
Wataru Watanabe <wataru.chocola@gmail.com>
2021-03-22 04:56:12 +0900

    clean up moinutil

--- tests/test_moinutils.py
+++ tests/test_moinutils.py
@@ -14,6 +14,22 @@ def test_parse_quoted_separated(data, expected):
 
 @pytest.mark.parametrize(
     ("data", "expected"), [
+        ('target="_blank",&do=get', [('target', '_blank'), ('&do', 'get')]),
+        ('target="_blank",&do=get,&test', [('target', '_blank'), ('&do', 'get'), '&test']),
+        ('target="_blank",&do=get ,a  ', [('target', '_blank'), ('&do', 'get'), 'a']),
+        ('a=b=c,"x=y"', [('a', 'b=c'), "x=y"]),
+        ('a=', [('a', '')]),
+        ('a="""b"', [('a', '"b')]),
+        ('width=100 height=150 xxx=11', [('width', '100 height=150 xxx=11')]),
+    ]
+)
+def test_parse_quoted_separated_ext(data, expected):
+    ret = moin2hugo.moinutils.parse_quoted_separated_ext(data)
+    assert ret == expected
+
+
+@pytest.mark.parametrize(
+    ("data", "expected"), [
         ('%E3%83%86%E3%82%B9%E3%83%88', 'テスト'),
     ]
 )
