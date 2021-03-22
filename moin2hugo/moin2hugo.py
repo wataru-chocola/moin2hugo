cafdf4acf3ce65c7bd58e990b21966e95a721c20
Wataru Watanabe <wataru.chocola@gmail.com>
2021-03-22 20:28:19 +0900

    implement cli entrypoint

--- moin2hugo/moin2hugo.py
+++ moin2hugo/moin2hugo.py
@@ -1,9 +1,14 @@
 import os
-
 import logging
 from dataclasses import dataclass
-from typing import Iterator, List
+from typing import Iterator, List, Optional
 
+import yaml
+import click
+
+from moin2hugo.config import load_config, Config
+from moin2hugo.moin_parser import MoinParser
+from moin2hugo.formatter import Formatter
 from moin2hugo.moinutils import unquoteWikiname
 
 logger = logging.getLogger(__name__)
@@ -23,7 +28,11 @@ class MoinPageInfo:
 
 
 class Moin2Hugo(object):
-    def __init__(self, src_dir: str, dst_dir: str):
+    def __init__(self, src_dir: str, dst_dir: str, config: Optional[Config] = None):
+        if config:
+            self.config = config
+        else:
+            self.config = Config()
         self.src_dir = src_dir
         self.dst_dir = dst_dir
 
@@ -34,6 +43,7 @@ class Moin2Hugo(object):
             if entry.name.startswith("."):
                 continue
 
+            logger.info("Page Found: %s" % unquoteWikiname(entry.name))
             pagedir = os.path.join(page_dir, entry.name)
             try:
                 current_file = os.path.join(pagedir, 'current')
@@ -44,6 +54,7 @@ class Moin2Hugo(object):
 
             content_file = os.path.join(pagedir, 'revisions', current_revision)
             if not os.path.isfile(content_file):
+                # TODO: page deleted?
                 logger.error("Not found: %s/revisions/%s" % (entry.name, current_revision))
                 continue
 
@@ -63,6 +74,47 @@ class Moin2Hugo(object):
             yield page
 
     def convert(self):
+        formatter = Formatter(config=self.config.hugo_config)
         for page in self.scan_pages(self.src_dir):
-            # TODO
-            pass
+            with open(page.filepath, 'r') as f:
+                content = f.read()
+            page_obj = MoinParser.parse(content, page.name,
+                                        site_config=self.config.moin_site_config)
+            logger.info("Convert Page: %s" % page.name)
+            logger.debug("Filepath: %s" % page.filepath)
+            converted = formatter.format(page_obj)
+            # TODO: do something
+            # TODO: page.attachments
+
+
+def config_logger(verbose: bool):
+    app_logger = logging.getLogger('moin2hugo')
+    app_logger.propagate = False
+    handler = logging.StreamHandler()
+    if verbose:
+        app_logger.setLevel(logging.DEBUG)
+        handler.setLevel(logging.DEBUG)
+    else:
+        app_logger.setLevel(logging.INFO)
+        handler.setLevel(logging.INFO)
+    app_logger.addHandler(handler)
+
+
+@click.command()
+@click.argument('src', type=click.Path(exists=True))
+@click.argument('dst', type=click.Path())
+@click.option('--config', '-c', 'configfile', type=click.Path(exists=True), default=None)
+@click.option('--verbose', '-v', 'verbose', type=bool, default=None, is_flag=True)
+def convert_site(src: str, dst: str, configfile: Optional[str], verbose: bool):
+    '''Convert moinmoin site directory into hugo's content directory.
+    '''
+    config_logger(verbose)
+    if configfile:
+        with open(configfile, 'r') as f:
+            config_data = f.read()
+        config_dict = yaml.safe_load(config_data)
+        config = load_config(config_dict)
+    else:
+        config = Config()
+    moin2hugo = Moin2Hugo(src, dst, config=config)
+    moin2hugo.convert()
