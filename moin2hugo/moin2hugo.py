1fb93336f1b9f6b9fd6ee96884a6b2aa62a8187d
Wataru Watanabe <wataru.chocola@gmail.com>
2021-03-29 16:41:03 +0900

    update README.md

--- moin2hugo/moin2hugo.py
+++ moin2hugo/moin2hugo.py
@@ -187,7 +187,7 @@ def config_logger(verbose: bool):
 @click.option('--config', '-c', 'configfile', type=click.Path(exists=True), default=None)
 @click.option('--verbose', '-v', 'verbose', type=bool, default=None, is_flag=True)
 def convert_site(src: str, dst: str, configfile: Optional[str], verbose: bool):
-    '''Convert moinmoin site directory to hugo content directory.
+    '''Convert MoinMoin site directory to Hugo content directory.
     '''
     config_logger(verbose)
     if configfile:
