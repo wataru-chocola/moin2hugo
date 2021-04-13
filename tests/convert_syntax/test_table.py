b00d05c491ef71f56807ac2e309d53c3c495f740
Wataru Watanabe <wataru.chocola@gmail.com>
2021-04-13 20:24:05 +0900

    add test cases

--- tests/convert_syntax/test_table.py
+++ tests/convert_syntax/test_table.py
@@ -1,6 +1,7 @@
 from moin2hugo.moin_parser import MoinParser
 from moin2hugo.formatter.hugo import HugoFormatter
 from moin2hugo.page_tree import Table, TableRow, TableCell
+from moin2hugo.config import HugoConfig
 
 import textwrap
 
@@ -74,6 +75,101 @@ def test_table_attrs():
     assert cell_22.attrs.colspan == 2, table.tree_repr()
 
 
-def test_extended_table():
-    # TODO
-    pass
+def test_detect_header_heuristically_1():
+    table_text = """\
+    || '''A''' || '''B''' || '''C''' ||
+    ||a ||b || c ||
+    """
+    expected = """\
+    | a | b | c |
+    | - | - | - |
+    | a | b | c |
+    """
+    data = textwrap.dedent(table_text)
+    page = MoinParser.parse(data, 'PageName')
+    expected = textwrap.dedent(expected)
+    config = HugoConfig(detect_table_header_heuristically=True)
+    assert HugoFormatter.format(page, config=config, pagename='PageName') == expected
+    assert page.source_text == data
+
+
+def test_detect_header_heuristically_2():
+    table_text = """\
+    ||<tablestyle="width: 90%;" rowstyle="width: 30%;" rowclass="header"> A || B || C ||
+    ||a ||b || c ||
+    """
+    expected = """\
+    | a | b | c |
+    | - | - | - |
+    | a | b | c |
+    """
+    data = textwrap.dedent(table_text)
+    page = MoinParser.parse(data, 'PageName')
+    expected = textwrap.dedent(expected)
+    config = HugoConfig(use_extended_markdown_table=True)
+    assert HugoFormatter.format(page, config=config, pagename='PageName') == expected
+    assert page.source_text == data
+
+
+def test_extended_table_without_extended_markdown_table():
+    table_text = """\
+    ||<tablestyle="width: 90%;" rowstyle="width: 30%;" rowclass="header"> A || B || C ||
+    ||a   ||<colspan=2>b ||
+    ||<^> ||b ||<rowspan=2>c ||
+    ||<^> ||b ||
+    """
+    expected = """\
+    | A | B | C |
+    | - | - | - |
+    | a | b |   |
+    |   | b | c |
+    |   | b |   |
+    """
+    data = textwrap.dedent(table_text)
+    page = MoinParser.parse(data, 'PageName')
+    expected = textwrap.dedent(expected)
+    assert HugoFormatter.format(page, pagename='PageName') == expected
+    assert page.source_text == data
+
+
+def test_normal_table_with_extended_markdown_table():
+    table_text = """\
+    ||A ||B ||C ||
+    ||a ||b ||c ||
+    """
+    expected = """\
+    |   |   |   |
+    | - | - | - |
+    | A | B | C |
+    | a | b | c |
+    """
+    data = textwrap.dedent(table_text)
+    page = MoinParser.parse(data, 'PageName')
+    expected = textwrap.dedent(expected)
+    config = HugoConfig(use_extended_markdown_table=True)
+    assert HugoFormatter.format(page, config=config, pagename='PageName') == expected
+    assert page.source_text == data
+
+
+def test_extended_table_with_extended_markdown_table():
+    table_text = """\
+    ||<tablestyle="width: 90%;" rowstyle="width: 30%;" rowclass="header"> A || B || C ||
+    ||a   ||<colspan=2>b ||
+    ||<^> ||b ||<rowspan=2>c ||
+    ||<^> ||b ||
+    """
+    expected = """\
+    {{< extended-markdown-table >}}
+    | A | B | C |
+    | - | - | - |
+    | a | > | b |
+    | ^ | b | c |
+    | ^ | b | ^ |
+    {{< /extended-markdown-table >}}
+    """
+    data = textwrap.dedent(table_text)
+    page = MoinParser.parse(data, 'PageName')
+    expected = textwrap.dedent(expected)
+    config = HugoConfig(use_extended_markdown_table=True)
+    assert HugoFormatter.format(page, config=config, pagename='PageName') == expected
+    assert page.source_text == data
