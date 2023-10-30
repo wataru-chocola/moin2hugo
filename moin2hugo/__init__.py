abfaf5969a1ede4d94d080a71d789edddcd28acb
Wataru Watanabe <wataru.chocola@gmail.com>
2023-10-30 09:59:15 +0900

    remove toml dependency

--- moin2hugo/__init__.py
+++ moin2hugo/__init__.py
@@ -1,11 +1,3 @@
-import os.path
+import importlib.metadata
 
-pytoml = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
-if os.path.isfile(pytoml):
-    import toml
-
-    __version__ = toml.load(open(pytoml))["tool"]["poetry"]["version"]
-else:
-    import pkg_resources
-
-    __version__ = pkg_resources.get_distribution("moin2hugo").version
+__version__ = importlib.metadata.version("moin2hugo")
